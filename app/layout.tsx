2882376653ca4ed66a92b37539064e3d198185c3
hot321ps-jpg <hot321ps@gmail.com>
2026-02-28 18:43:53 +0800

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "./globals.css";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="zh-Hant">
-      <body>{children}</body>
-    </html>
-  );
-}
